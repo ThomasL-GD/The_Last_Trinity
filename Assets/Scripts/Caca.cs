3795a7f4e2ba8a9f37509f19b4db34bfdfa4d6e0
ThomasL-GD <59962750+ThomasL-GD@users.noreply.github.com>
2021-04-08 15:00:27 +0200

    Update Caca.cs

--- Assets/Scripts/Caca.cs
+++ Assets/Scripts/Caca.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Caca : MonoBehaviour
-{
-    
-    [SerializeField] private float 
+public class Caca : MonoBehaviour {
+
+    [SerializeField] private float m_speed = 0.0f;
     
     // Start is called before the first frame update
     void Start()
