eec807d32b5bab2ed4f46781dabd2c6f0582b28b
ThomasL-GD <59962750+ThomasL-GD@users.noreply.github.com>
2021-04-21 16:49:26 +0200

    Multi-Chara yay !

--- Assets/Scenes/Tests_code/Movement/Script/PlayerController.cs
+++ Assets/Scenes/Tests_code/Movement/Script/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,31 +9,60 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
-    [Tooltip("Vitesse du joueur")] public float m_speed = 5f;
-    [Tooltip("Vitesse de Rotation du Quaternion")] public float m_rotationSpeed = 700f;
-    [Tooltip("The input used to select this character")] public KeyCode m_selector = KeyCode.Space;
+
+    [Flags]
+    public enum Charas {
+        Human = 0,
+        Monster = 1,
+        Robot = 2
+    }
+
+    [SerializeField] [Tooltip("Vitesse du joueur")] private float m_speed = 5f;
+    [SerializeField] [Tooltip("Vitesse de Rotation du Quaternion")] private float m_rotationSpeed = 700f;
+    [SerializeField] [Tooltip("The input used to select this character")] private SOInputMultiChara m_selector = null;
+    [SerializeField] [Tooltip("The character whom this script is on")] private Charas m_chara = 0;
+    private KeyCode[] m_keyCodes = new[] {KeyCode.Joystick1Button0, KeyCode.Joystick1Button3, KeyCode.Joystick1Button1};
     private bool m_isActive = false;
-    
+
+    private void Start() {
+        //We create an array (because it's easier to manipulate) of all the inputs of the characters
+        m_keyCodes[0] = m_selector.inputHuman;
+        m_keyCodes[1] = m_selector.inputMonster;
+        m_keyCodes[2] = m_selector.inputRobot;
+
+        if (m_chara == Charas.Human) m_isActive = true;
+    }
+
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
-        Vector3 movementDirection = new Vector3(horizontalInput,  0, verticalInput);
-        movementDirection.Normalize();
-                
-        transform.Translate(movementDirection * m_speed * Time.deltaTime, Space.World);
-        
-        //Utilisation du Quaternion pour permettre au player de toujours se déplacer dans l'angle où il regarde
-        if (movementDirection != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+        //The character is not able to move if not selected
+        if (m_isActive) {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            
+            Vector3 movementDirection = new Vector3(horizontalInput,  0, verticalInput);
+            movementDirection.Normalize();
                     
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, m_rotationSpeed * Time.deltaTime);
+            if(m_isActive) transform.Translate(movementDirection * m_speed * Time.deltaTime, Space.World);
+        
+            //Utilisation du Quaternion pour permettre au player de toujours se déplacer dans l'angle où il regarde
+            if (movementDirection != Vector3.zero)
+            {
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+                        
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, m_rotationSpeed * Time.deltaTime);
+            }
+            
         }
 
-        if (Input.GetKeyDown(m_selector)) {
+        //We activate this chara if its corresponding input is pressed
+        if (Input.GetKeyDown(m_keyCodes[(int)m_chara])) {
             m_isActive = true;
+        }
+        //If any other input corresponding to a character is pressed, we inactive this chara
+        else if (Input.GetKeyDown(m_keyCodes[0]) || Input.GetKeyDown(m_keyCodes[1]) || Input.GetKeyDown(m_keyCodes[2])){
+            Debug.Log("whatever");
+            m_isActive = false;
         }
     }
 }
