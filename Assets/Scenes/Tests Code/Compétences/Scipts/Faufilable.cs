4a598d235730713de92d436770c3ef3fffd6b8ae
Arthur Sir <35264395+CrocoDeals@users.noreply.github.com>
2021-05-06 17:49:12 +0200

    Proto V0.5

--- Assets/Scenes/Tests Code/Compétences/Scipts/Faufilable.cs
+++ Assets/Scenes/Tests Code/Compétences/Scipts/Faufilable.cs
@@ -5,47 +5,89 @@ using UnityEngine;
 public class Faufilable : MonoBehaviour
 {
     [SerializeField] [Tooltip("The input used to select this character")] private SOInputMultiChara m_selector = null;
-    private bool m_faufilableOpen = false; //Possibilité d'activer le Faufilage avec la touche de compétence du Humain
-    private bool m_activeFaufilage = false;
-    private bool m_isHidden = false;
-
-    private Vector3 m_originalPos;
-    private Vector3 m_targetPos;
-    [SerializeField] [Tooltip("Time of Travel")] [Range(0.1f, 3f)] private float m_smoothTime = 1f;
-    [SerializeField] [Tooltip("Height of Travel")] [Range(1f, 30f)] private float m_teleVal = 10f;
+    private bool m_isOpenToFaufilage = false; //Possibilité d'activer le Faufilage avec la touche de compétence du Humain
+    private bool m_isIntoWall= false; //Est en standby dans l'autre mur
     
-    [SerializeField] [Tooltip("Un slider d'int")] [Range(0, 4)] private int m_unSliderDint = 2;
-    //[SerializeField] [Tooltip("The maximum authorized difference between the position to reach and the current position (unit : Unity meters)")] [Range(0f, 1f)] private float m_uncertainty = 0.1f;
-    private Vector3 m_velocity = Vector3.zero;
+    [SerializeField] private Transform m_exit = null;
 
-    private PlayerController m_humanScript = null;
-    
+    [Header("Travel Timing")]
+    [SerializeField] [Tooltip("Time of animation")] [Range(0.1f, 3f)] private float m_animTime = 1f; //Temps pour l'éxécution de l'animation de faufilage
+    [SerializeField] [Tooltip("Time of travel between the two exits")] [Range(0.1f, 3f)] private float m_travelTime = 1f; //Temps avant que le joueur se téléporte vers la sortie
+    [SerializeField] [Tooltip("Offset for getting out of the exit")] [Range(0.1f, 3f)] private float m_offset = 0.2f; //Offset du joueur pour le placer dans la fissure
 
+    [SerializeField] [Tooltip("For Debug Only")] private Transform m_human = null;
+    [SerializeField] [Tooltip("For Debug Only")] private PlayerController m_humanScript = null;//Script de l'humain, obtenir la touche d'activation de la compétence
 
     void Start()
     {
-        m_originalPos = transform.position;
-        // Define a target position above the object transform
-        m_targetPos = new Vector3(m_originalPos.x, m_originalPos.y + m_teleVal, m_originalPos.z);
-
         if (m_selector == null)
         {
             Debug.LogError("Manque le scriptable object d'input");
-        }    
+        }
+
+        if (m_exit == null)
+        {
+            Debug.LogError("Le Transform n'est pas sérialisé");
+        }
     }
 
     private void Update()
     {
-        Vector3 posClamp = ClampEnjoyer(transform.position);
-        
-        if (m_faufilableOpen)
-        {
-            if (Input.GetKeyDown(m_selector.inputRobot) && !m_isHidden && m_humanScript.m_isActive)
+        if (m_isOpenToFaufilage)
+        { ;
+            if (!m_isIntoWall && m_humanScript.m_isActive && Input.GetKeyDown(m_selector.inputHuman))
             {
-                m_isHidden = true;
-                if (m_activeFaufilage) m_humanScript.m_isForbiddenToMove = true;
+                Debug.Log("Begin");
+                m_humanScript.m_isForbiddenToMove = true;
+                StartCoroutine(AnimationEntree());
             }
         }
+
+        if (m_isIntoWall && Input.GetKeyDown(m_selector.inputHuman))
+        {
+            StartCoroutine(AnimationSortie());
+        }
+    }
+
+    /// <summary>
+    /// Laisse le temps à l'animation de se jouer pour passer dans le mur
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AnimationEntree()
+    {
+        Debug.Log("Animation Enter");
+        //Animation Play()
+        yield return new WaitForSeconds(m_animTime);
+        StartCoroutine(Teleport());
+    }
+
+    /// <summary>
+    /// Teleporte le joueur sur la sortie sérialisé
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Teleport()
+    {
+        Debug.Log("Teleport");
+        yield return new WaitForSeconds(m_travelTime);
+        m_human.transform.position = m_exit.transform.position;
+        m_isIntoWall = true;
+    }
+    
+    /// <summary>
+    /// Laisse le temps à l'animation de se jouer pour sortir du mur
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator AnimationSortie()
+    {
+        Debug.Log("Animation Sortie");
+        //Animation Play()
+        yield return new WaitForSeconds(m_animTime);
+        m_humanScript.m_isForbiddenToMove = false;
+        m_isIntoWall = false;
+        
+        //Reset des valeurs
+        m_human = null;
+        m_humanScript = null;
     }
 
     /// <summary>
@@ -58,7 +100,8 @@ public class Faufilable : MonoBehaviour
         {
             if (player.m_chara == Charas.Human)
             {
-                m_faufilableOpen = true;
+                m_isOpenToFaufilage = true;
+                m_human = p_other.gameObject.transform;
                 m_humanScript = player;
             }
         }
@@ -72,19 +115,12 @@ public class Faufilable : MonoBehaviour
     {
         if (p_other.gameObject.TryGetComponent(out PlayerController player))
         {
-            if (player.m_chara == Charas.Robot)
+            Debug.Log("Prout");
+            if (player.m_chara == Charas.Human)
             {
-                m_faufilableOpen = false;
-                m_activeFaufilage = false;
-                m_humanScript = null;
+                m_isOpenToFaufilage = false;
                 //Debug.Log(m_telekinesieOpen);
             }
         }
-    }
-
-    private Vector3 ClampEnjoyer(Vector3 p_pos)
-    {
-        float pow = Mathf.Pow(10, m_unSliderDint);
-        return new Vector3(Mathf.Round((p_pos.x * pow)/pow), Mathf.Round((p_pos.y * pow)/pow), Mathf.Round((p_pos.z * pow)/pow));
     }
 }
