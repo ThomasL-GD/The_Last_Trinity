58b465f7488c893971ce83be7a9841e0f8d95c34
Arthur Sir <35264395+CrocoDeals@users.noreply.github.com>
2021-05-18 14:18:41 +0200

    Adding some props

--- Assets/Scenes/Tests Code/Prototype/Scripts/ZoneChangementDolly.cs
+++ Assets/Scenes/Tests Code/Prototype/Scripts/ZoneChangementDolly.cs
@@ -8,12 +8,33 @@ public class ZoneChangementDolly : MonoBehaviour
 {
     //Cinemachine cameras des trois personnages zoomées sur un rail auxiliaire
     //Effectuer un blend dans la "Main Camera" avant de régler les priorités
-    [SerializeField][Tooltip("Virtual Caméra Zoomée")] private CinemachineVirtualCamera m_vCamHZ;
-    [SerializeField][Tooltip("Virtual Caméra Zoomée")] private CinemachineVirtualCamera m_vCamMZ;
-    [SerializeField][Tooltip("Virtual Caméra Zoomée")] private CinemachineVirtualCamera m_vCamRZ;
+    [SerializeField][Tooltip("Virtual Caméra Zoomée")] private CinemachineVirtualCamera m_vCamHZ = null;
+    [SerializeField][Tooltip("Virtual Caméra Zoomée")] private CinemachineVirtualCamera m_vCamMZ = null;
+    [SerializeField][Tooltip("Virtual Caméra Zoomée")] private CinemachineVirtualCamera m_vCamRZ = null;
+    
+    //Stocke l'ancienne caméra pour pouvoir y retourner quand le joueur sort de la zone
+    private CinemachineVirtualCamera m_previousVCamH;
+    private CinemachineVirtualCamera m_previousVCamM;
+    private CinemachineVirtualCamera m_previousVCamR;
+
+    private void Start()
+    {
+        if(m_vCamHZ == null)
+        {
+            Debug.LogError("La camera humaine n'est pas serializée");
+        }
+        if(m_vCamMZ == null)
+        {
+            Debug.LogError("La camera monstre n'est pas serializée");
+        }
+        if(m_vCamRZ == null)
+        {
+            Debug.LogError("La camera robot n'est pas serializée");
+        }
+    }
 
     /// <summary>
-    /// Tigger enter de la zone à zoomer avec le changement de priorité à 3 pour avoir la priorité
+    /// Change the character's current camera to the zoomed one and stocks the old one for later
     /// </summary>
     /// <param name="p_other">Collider du joueur entrant dans la zone</param>
     private void OnTriggerEnter(Collider p_other)
@@ -21,24 +42,26 @@ public class ZoneChangementDolly : MonoBehaviour
         Debug.Log("Ok");
         if (p_other.gameObject.TryGetComponent(out PlayerController charaScript))
         {
-            Debug.Log(charaScript.m_chara);
             switch (charaScript.m_chara)
             {
                 case Charas.Human :
-                    m_vCamHZ.Priority = 3;
+                    m_previousVCamH = charaScript.GetCurrentCamera();
+                    charaScript.SetNewCamera(m_vCamHZ);
                     break;
                 case Charas.Monster :
-                    m_vCamMZ.Priority = 3;
+                    m_previousVCamM = charaScript.GetCurrentCamera();
+                    charaScript.SetNewCamera(m_vCamMZ);
                     break;
                 case Charas.Robot :
-                    m_vCamRZ.Priority = 3;
+                    m_previousVCamR = charaScript.GetCurrentCamera();
+                    charaScript.SetNewCamera(m_vCamRZ);
                     break;
             }   
         }
     }
-    
+
     /// <summary>
-    /// Tigger exit de la zone à zoomer avec le changement de priorité à 1 pour enlever la priorité
+    /// Puts back the previous camera to any character going out of the zone
     /// </summary>
     /// <param name="p_other">Collider du joueur entrant dans la zone</param>
     private void OnTriggerExit(Collider p_other)
@@ -50,13 +73,16 @@ public class ZoneChangementDolly : MonoBehaviour
             switch (charaScript.m_chara)
             {
                 case Charas.Human :
-                    m_vCamHZ.Priority = 1;
+                    charaScript.SetNewCamera(m_previousVCamH);
+                    m_previousVCamH = m_vCamHZ;
                     break;
                 case Charas.Monster :
-                    m_vCamMZ.Priority = 1;
+                    charaScript.SetNewCamera(m_previousVCamM);
+                    m_previousVCamM = m_vCamMZ;
                     break;
                 case Charas.Robot :
-                    m_vCamRZ.Priority = 1;
+                    charaScript.SetNewCamera(m_previousVCamR);
+                    m_previousVCamR = m_vCamRZ;
                     break;
             }   
         }
