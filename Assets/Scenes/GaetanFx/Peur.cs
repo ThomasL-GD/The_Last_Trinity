2caf1f4495ccfb863bad01229b7aaa7543756819
Blue <59962750+ThomasL-GD@users.noreply.github.com>
2021-06-03 11:14:05 +0200

    Script anim

--- Assets/Scenes/GaetanFx/Peur.cs
+++ Assets/Scenes/GaetanFx/Peur.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Peur : MonoBehaviour
 {
+
+    private Animator m_animator = null;
+    private static readonly int peur = Animator.StringToHash("Peur");
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_animator.SetTrigger(peur);
+        }
     }
 }
