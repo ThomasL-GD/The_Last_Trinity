8c2b3d94385458b1e921ae8bf58d31a374d979b5
Blue <59962750+ThomasL-GD@users.noreply.github.com>
2021-06-03 15:39:55 +0200

    Y'a moy la soul est réparée :ghost:

--- Assets/Scenes/Tests Code/Completed Scene/Changement entre Personnages (Scriptable Input)/Script/AutoRotation.cs
+++ Assets/Scenes/Tests Code/Completed Scene/Changement entre Personnages (Scriptable Input)/Script/AutoRotation.cs
@@ -10,7 +10,7 @@ public class AutoRotation : MonoBehaviour {
     [SerializeField] [Range(0.01f, 2f)] [Tooltip("The time taken for the object to go to its target (unit : seconds)")] public float m_duration = 2.0f;
     private float m_durationLeft = 2.0f;
     [SerializeField] [Range(0.0f, 1.2f)] [Tooltip("The distance between this object and its target at which this object will auto-destroy (unit : meters)")] private float m_uncertainty = 0.2f;
-    [HideInInspector] public Transform m_target = null;
+    /*[HideInInspector]*/ public Transform m_target = null;
     [HideInInspector] public Vector3 m_offsetTarget = Vector3.zero;
     private bool m_targetNull = true;
     private VisualEffect m_visualEffect = null;
@@ -39,23 +39,26 @@ public class AutoRotation : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        //If we previously had no target but now we have one
         if (m_targetNull && m_target != null) {
             m_targetNull = false;
             m_durationLeft = m_duration;
             m_visualEffect.Play();
         }
 
+        //
         if (!m_targetNull) {
             Transform transform1;
             (transform1 = transform).Rotate(Vector3.up, m_rotationStrength);
 
-            var position = transform1.position;
-            var positionTarget = m_target.position;
-            transform.position = position + (positionTarget + m_offsetTarget - position).normalized * Time.deltaTime * (Mathf.Abs((positionTarget + m_offsetTarget - position).magnitude) / m_durationLeft);
+            Vector3 position = transform1.position;
+            Vector3 positionTarget = m_target.position;
+            transform.position = position + (positionTarget + m_offsetTarget - position).normalized * (Time.deltaTime * (Mathf.Abs((positionTarget + m_offsetTarget - position).magnitude) / m_durationLeft));
 
             m_durationLeft -= Time.deltaTime;
             
-            if (Mathf.Abs((positionTarget + m_offsetTarget - transform.position).magnitude) <= m_uncertainty) {
+            //if (Mathf.Abs((positionTarget + m_offsetTarget - transform.position).magnitude) <= m_uncertainty) {
+            if (m_durationLeft < 0 || (Mathf.Abs((positionTarget + m_offsetTarget - transform.position).magnitude) <= m_uncertainty)) {
                 m_target = null;
                 m_targetNull = true;
                 m_visualEffect.Stop();
