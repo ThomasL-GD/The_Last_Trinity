f2c8e500b01c7a52a4d0d8b68aa510507d2c0859
Blue <59962750+ThomasL-GD@users.noreply.github.com>
2021-06-07 23:30:57 +0200

    The game is technically complete now :100:

--- Assets/Scenes/Tests Code/Prototype/Scripts/CreditsPage.cs
+++ Assets/Scenes/Tests Code/Prototype/Scripts/CreditsPage.cs
@@ -9,19 +9,21 @@ public class CreditsPage : MonoBehaviour
     [Header("Audio")] 
     [SerializeField] [Tooltip("Son d'ambiance")] private AudioSource m_mainTheme = null;
 
+    private bool m_areCreditsVisible = false;
+
     // Start is called before the first frame update
     void Start()
     {
         if ( m_mainTheme != null && !m_mainTheme.isPlaying) m_mainTheme.Play();
+        DeathManager.Instance.DeathFade(true, 0.01f);
+        DeathManager.OnTransparentScreen += DisplayedCredits;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.JoystickButton9)) {
+        if (m_areCreditsVisible  && Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.JoystickButton9)) {
 
-            DeathManager.Instance.DeathFade(false, 1f);
-            DeathManager.OnBlackScreen = null;
             DeathManager.OnBlackScreen += LaunchMainMenu;
             DeathManager.Instance.DeathFade(false, 1f);
         }
@@ -29,6 +31,11 @@ public class CreditsPage : MonoBehaviour
 
     public void LaunchMainMenu() {
         PlayerPrefs.SetInt("Level", SceneManager.GetActiveScene().buildIndex + 1);
+        PlayerPrefs.Save();
         SceneManager.LoadScene(0);
+    }
+    
+    private void DisplayedCredits() {
+        m_areCreditsVisible = true;
     }
 }
